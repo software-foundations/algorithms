2287fdddfa81d45af6ba6932f702eb7fd803bb23
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-11-05 12:49:52 -0300

    (sort_and_search, and sort_and_search.c, at /sort_and_search) -> C qsort built-int usage

--- sort_and_search/sort_and_search.c
+++ sort_and_search/sort_and_search.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// not working with qsort
 int intcompare(int *i, int *j)
 {
     if (*i > *j) return (1);
@@ -28,14 +29,39 @@ int test_intcompare()
     return (0);
 }
 
+// working with qsort
+int compare_version_2(const void *x_void, const void *y_void)
+{
+    // cast the void pointer to a int pointer
+    int x = *(int *)x_void;
+    int y = *(int *)y_void;
+    return x - y;
+}
+
 int main()
 {
     
     test_intcompare();
 
-    // Sorting
-    // Not working yet
-    int a[11] = {0, 1, 3, 5, 7, 2 , 4, 9, 8, 10, 6};
-    int n = 1;    
-    qsort(a, n, sizeof(int), intcompare);
+    // Sorting    
+    int a[] ={8, 7, 2, 4, 6, 3, 5, 1, 9, 0};
+    int b[] ={8, 7, 2, 4, 6, 3, 5, 1, 9, 0};
+    int lenght = 10;
+
+    qsort(a, lenght, sizeof(int), compare_version_2);
+
+    for (int i = 0; i < lenght; i++)
+    {
+        printf("a[%d] = %d\n", i, a[i]);
+    }
+
+    qsort(b, lenght, sizeof(int), compare_version_2);
+
+    for (int j = 0; j < lenght; j++)
+    {
+        printf("b[%d] = %d\n", j, b[j]);
+    }
+
+    return 0;
+
 }
